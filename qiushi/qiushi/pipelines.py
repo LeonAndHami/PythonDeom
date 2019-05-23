8216b29d8c1f7885edaadda84d6993732b101cce
Leon <799686872@qq.com>
2019-05-23 23:26:51 +0800

    糗事 CrawlSpider 完善

--- qiushi/qiushi/pipelines.py
+++ qiushi/qiushi/pipelines.py
@@ -5,9 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+
 
 class QiushiPipeline(object):
     def process_item(self, item, spider):
         # print(item)
-        print("QiushiPipeline")
+        # with open("./qiushi.txt", "a", encoding="utf8")as f:
+        #     f.write(json.dumps(item, ensure_ascii=False, indent=4))
+        #     f.write("\n")
         return item
--- qiushi/qiushi/spiders/qs.py
+++ qiushi/qiushi/spiders/qs.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+import copy
 
 
 class QsSpider(CrawlSpider):
@@ -10,11 +11,22 @@ class QsSpider(CrawlSpider):
     start_urls = ['https://www.qiushibaike.com/text/page/1/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'https://www.qiushibaike.com/text/page/\d{1,2}/'), callback='parse_item',
-             follow=False),
+        # 这里可以加个详情页的规则
+        Rule(LinkExtractor(allow=r'https://www.qiushibaike.com/text/page/\d+/'), callback='parse_item',
+             follow=True),
     )
+    """
+    流程：
+    1、请求start_urls
+    2、rules 规则提取url（提取到的url不一定是第1页）
+    3、进入 parse_item方法
+    4、parse_item 退出，follow=true，提取下一页url,进行下一轮循环
+    
+    可以改成从每个帖子的详情页中提取，但这样请求量就增加了。
+    """
 
     def parse_item(self, response):
+        print(response.request.url)
         divs = response.xpath('//div[@id="content-left"]/div')
         for div in divs:
             item = {}
@@ -25,10 +37,12 @@ class QsSpider(CrawlSpider):
             if item['content'][-1] == "查看全文":
                 url = "https://www.qiushibaike.com" + div.xpath(
                     ".//a[@class='contentHerf']/@href").extract_first()
-                yield scrapy.Request(url, callback=self.get_detail, meta=item)
+                yield scrapy.Request(url, callback=self.get_detail, meta=copy.deepcopy(item))
             yield item
 
-
     def get_detail(self, response):
-
-        pass
+        item = response.meta
+        item['content'] = [x.replace("\n", "").replace("\u3000", "") for x in
+                           response.xpath("//div[@class='content']/text()").extract()]
+        # print(item)  # 通过meta传过来之后多几个属性？
+        yield item
