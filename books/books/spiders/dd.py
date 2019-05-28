99eaa4708f6a81dfc3610ef04788d0d551076c8a
Leon <799686872@qq.com>
2019-05-28 23:33:47 +0800

    book 5-28

--- books/books/spiders/dd.py
+++ books/books/spiders/dd.py
@@ -12,5 +12,25 @@ class DdSpider(scrapy.Spider):
         # with open('d:/boo.txt','w',encoding='utf8') as f:
         #     f.write(response.text)
         # 2-- 11
-
+        for div in divs:
+            item = {}
+            item["level_one"] = [x.strip() for x in div.xpath("./dl/dt//text()").extract() if len(x.strip()) > 0][0]
+            dls = div.xpath(".//dl[@class='inner_dl']")
+            print(len(dls))
+            count = 1
+            for dl in dls:
+                # with open("d:/temp/dl"+str(count)+".txt","w",encoding="utf8") as f:
+                #     f.write(dl.extract())
+                item["level_two"] = [x.strip() for x in dl.xpath("./dt/a/text()").extract()]
+                if item["level_two"] is None:
+                    item["level_two"] = dl.xpath("./dt[position()=1]//text()").extract_first()
+                # count += 1
+                # a_list = dl.xpath("./dd/a")
+                # for a in a_list:
+                #     item["level_three"] = a.xpath("./text()").extract_first()
+                print(item)
+                print("*" * 20)
+            # return
+            # print(item)
+            # print("*" * 20)
         pass
